17bccdef7f998e027b363aa0c89c1d92d675eeb7
jason <996335280@qq.com>
2023-04-01 00:02:33 +0800

    改变图片位置

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
-
-cd ./service
-nohup pnpm start > service.log &
-echo "Start service complete!"
-
-
-cd ..
 echo "" > front.log
-nohup pnpm dev > front.log &
+nohup pnpm serve > front.log &
 echo "Start front complete!"
 tail -f front.log
