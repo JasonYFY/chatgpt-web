c91c6381ae23ecd7a2410f65ded1df46bb1e989b
jason <996335280@qq.com>
2023-04-01 12:19:33 +0800

    第6次尝试

---
+++ src/components/common/UserAvatar/index.vue
@@ -0,0 +1,25 @@
+<script setup lang='ts'>
+import { computed } from 'vue'
+import { NAvatar } from 'naive-ui'
+import { isString } from '@/utils/is'
+
+
+</script>
+
+<template>
+  <div class="flex items-center overflow-hidden">
+    <div class="w-10 h-10 overflow-hidden rounded-full shrink-0">
+      <template>
+        <NAvatar size="large" round :src="/avatar.jpg" />
+      </template>
+    </div>
+    <div class="flex-1 min-w-0 ml-2">
+      <h2 class="overflow-hidden font-bold text-md text-ellipsis whitespace-nowrap">
+        JasonYu
+      </h2>
+      <p class="overflow-hidden text-xs text-gray-500 text-ellipsis whitespace-nowrap">
+
+      </p>
+    </div>
+  </div>
+</template>
