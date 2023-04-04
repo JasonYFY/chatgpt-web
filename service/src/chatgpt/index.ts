e9943f9ac13ca2f874cb9f00a9823250f8c62d33
jason <996335280@qq.com>
2023-04-04 23:38:56 +0800

    改变位置2

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -140,16 +140,17 @@ async function chatReplyProcess(options: RequestOptions) {
 
 			console.log('Client IP:', clientIP) // 打印客户端IP地址
 
+			// 将客户端IP地址存储到LRUMap中
+			if (!ipToken) {
+				//没有在缓存里,获取一个新的保存
+				ipToken = loadBalancer(accessTokens)()
+				ipCache.set(clientIP, ipToken)
+				console.log('新ip保存下token:',ipToken)
+			}
+			//重新赋值
+			(api as ChatGPTUnofficialProxyAPI).accessToken = ipToken
 			while (!response && retryCount++ < maxRetry) {
-				// 将客户端IP地址存储到LRUMap中
-				if (!ipToken) {
-					//没有在缓存里,获取一个新的保存
-					ipToken = loadBalancer(accessTokens)()
-					ipCache.set(clientIP, ipToken)
-					console.log('新ip保存下token:',ipToken)
-				}
-				//重新赋值
-				(api as ChatGPTUnofficialProxyAPI).accessToken = ipToken
+
 				// nextKey()
 				response = await api.sendMessage(message, options).catch((error: any) => {
 					// 429 Too Many Requests
