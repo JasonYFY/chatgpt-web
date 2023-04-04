112e5ea535de7b6197a93f3be77a06a9c5f773cb
jason <996335280@qq.com>
2023-04-05 01:05:51 +0800

    日志打印8

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -150,17 +150,18 @@ async function chatReplyProcess(options: RequestOptions) {
 				}
 				//重新赋值
 				(api as ChatGPTUnofficialProxyAPI).accessToken = ipToken
-				await api.sendMessage(message, options).catch((error: any) => {
+				response = await api.sendMessage(message, options).catch((error: any) => {
 					// 429 Too Many Requests
-					if (error.statusCode !== 429)
-						throw error
-					/*if (error.statusCode === 404){
+					if (error.statusCode === 404){
 						console.log('报错了404',error)
-						//console.log('报错了404，options：',options)
+						console.log('报错了404，options：',options)
+						const { conversationId, parentMessageId, ...rest } = options;
+						options = { ...rest };
+						console.log('报错了404，options新对象：',options)
 					}else if (error.statusCode !== 429)
-						throw error*/
+						throw error
 
-				}) = response
+				})
 				console.log('报错了重新执行',retryCount)
 				await sleep(retryIntervalMs)
 			}
