93e3fb37772e4b899587cc35727b71dd5c3e740f
yifangyu <fangyu.yi@jdh.com.cn>
2023-04-21 10:40:08 +0800

    fix3-适配多个自建代理

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -162,6 +162,7 @@ async function chatReplyProcess(options: RequestOptions) {
 				}
 				//重新赋值
 				console.log(typeof ipInfo);
+				console.log(ipInfo.ipToken);
 				(api as ChatGPTUnofficialProxyAPI).accessToken = ipInfo.ipToken
 				(api as ChatGPTUnofficialProxyAPI).apiReverseProxyUrl = ipInfo.ipProxy
 				console.log('打印下api:',api)
