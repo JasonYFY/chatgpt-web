180863773d065c427c3a7ee5be50dc7f6cdbe9e7
yifangyu <fangyu.yi@jdh.com.cn>
2023-04-21 12:08:19 +0800

    fix7-适配多个自建代理

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -149,26 +149,21 @@ async function chatReplyProcess(options: RequestOptions) {
 
 			while (!response && retryCount++ < maxRetry) {
 				// 将客户端IP地址存储到LRUMap中
-				let ipToken;
-				let ipProxy;
 				if (!ipInfo) {
 					//没有在缓存里,获取一个新的保存
-					ipToken = nextBalancer()
-					ipProxy = nextProxy()
+					let ipToken = nextBalancer()
+					let ipProxy = nextProxy()
 					ipInfo = {
 						ipToken: ipToken,
 						ipProxy: ipProxy
 					}as IPInfo;
 					ipCache.set(clientIP, ipInfo)
 					console.log(`新ip保存下token:${ipToken},新的proxyUrl:${ipProxy}`);
-				}else{
-					ipToken=ipInfo.ipToken
-					ipProxy=ipInfo.ipProxy
 				}
 
 				//重新赋值
-				(api as ChatGPTUnofficialProxyAPI).accessToken = ipToken
-				(api as ChatGPTUnofficialProxyAPI).apiReverseProxyUrl = ipProxy
+				(api as ChatGPTUnofficialProxyAPI).accessToken = getToken(ipInfo)
+				(api as ChatGPTUnofficialProxyAPI).apiReverseProxyUrl = getProxy(ipInfo)
 				console.log('打印下api:',api)
 				response = await api.sendMessage(message, options).catch((error: any) => {
 					// 429 Too Many Requests
@@ -205,6 +200,14 @@ async function chatReplyProcess(options: RequestOptions) {
       return sendResponse({ type: 'Fail', message: ErrorCodeMessage[code] })
     return sendResponse({ type: 'Fail', message: error.message ?? 'Please check the back-end console' })
   }
+}
+
+function getToken(ipInfo){
+	return ipInfo.ipToken
+}
+
+function getProxy(ipInfo){
+	return ipInfo.ipProxy
 }
 
 async function fetchBalance() {
