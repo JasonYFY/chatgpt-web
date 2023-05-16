2a1a9efaaf27ee2891edd3aa1d9e2d7e59ca5cdb
JasonYFY <996335280@qq.com>
2023-05-16 18:25:40 +0800

    feat:若有sql语句，则需单引号替换成双引号才行

--- service/src/utils/mindsdb.ts
+++ service/src/utils/mindsdb.ts
@@ -23,17 +23,20 @@ export async function sendMindDB(msg: string) {
 	console.log('去问MindsDB：',msg);
 	var mysql = require('mysql');
 	const query = `SELECT response FROM mindsdb.gpt4hassio WHERE text=${mysql.escape(msg)}`;
-	console.log('MindsDB的SQL：',query);
+	//若有sql语句，则需单引号替换成双引号才行
+	let newQuery = query.replace(/'/g, '"');
+	console.log('MindsDB的SQL：',newQuery);
 	let matchingUserRow = '';
 	try {
-		//console.log('查询的sql：',query);
-		const queryResult = await MindsDB.SQL.runQuery(query);
+		const queryResult = await MindsDB.SQL.runQuery(newQuery);
 		console.log('MindsDB的响应：',queryResult);
 		if (queryResult.rows.length > 0) {
 			matchingUserRow = queryResult.rows[0];
 			//console.log('查询MindsDB的值：',matchingUserRow);
 		}
-		matchingUserRow = queryResult;
+		if(queryResult.error_message){
+			matchingUserRow = queryResult.error_message;
+		}
 	} catch (error) {
 		console.error('查询MindsDB报错了：',error);
 		throw error;
