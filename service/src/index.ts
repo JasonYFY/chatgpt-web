7b51bf3315a8a93087c564b03083c3880d307866
JasonYFY <996335280@qq.com>
2023-05-29 14:36:32 +0800

    test

--- service/src/index.ts
+++ service/src/index.ts
@@ -25,6 +25,8 @@ router.post('/chat-process', [auth, limiter], async (req, res) => {
     const { prompt, options = {}, systemMessage, temperature, top_p,usingGpt4 } = req.body as RequestProps
 		console.log('x-forwarded-for的信息：',req.headers['x-forwarded-for'])
 		console.log('socket.remoteAddress的信息：',req.socket.remoteAddress)
+		console.log('connection.remoteAddress的信息：',req.connection.remoteAddress)
+		console.log('req.ip的信息：',req.ip)
 
 		//获取客户端ip
 		const clientIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress
