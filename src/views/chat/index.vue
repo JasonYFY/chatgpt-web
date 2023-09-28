20f3de50c0eab74784d17ba0b5524c8533587099
yifangyujason <996335280@qq.com>
2023-09-28 09:34:27 +0800

    Update index.vue

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -122,7 +122,7 @@ async function onConversation() {
     +uuid,
     {
       dateTime: new Date().toLocaleString(),
-      text: '思考中',
+      text: '',
       loading: true,
       inversion: false,
       error: false,
