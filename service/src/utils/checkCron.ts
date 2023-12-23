420fae7aa5a4875d81c160ef3ff316a60e02dca0
yifangyujason <996335280@qq.com>
2023-12-24 00:44:49 +0800

    Update checkCron.ts

--- service/src/utils/checkCron.ts
+++ service/src/utils/checkCron.ts
@@ -87,15 +87,8 @@ async function checkTokenExpires() {
 				//console.log('userInfoMap:',...userInfoMap);
 				//取出参数
 				console.log('准备执行的email：',email.trim());
-				const userInfo = userInfoMap.get(email.trim().toString());
+				const userInfo = userInfoMap.get(email.trim());
 				console.log('准备执行的userInfo：',userInfo);
-
-				const dd = 'yifangyuJason@gmail.com'
-				console.log('准备执行的email===dd：',email===dd);
-				console.log('准备执行的email===dd：',email.trim()===dd);
-				console.log('准备执行的email===dd：',email.toString()===dd);
-				const userInfo2 = userInfoMap.get(dd)
-				console.log('准备执行的userInfo2：',userInfo2);
 				if(userInfo){
 					var accessToken;
 					if(userInfo.type){
